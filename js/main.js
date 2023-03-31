016184df9e1cf4d2f89ccdd435e3b7cf0699b4be
Stephen Dunlap <68578271+MrSDunlap@users.noreply.github.com>
2023-03-31 18:41:37 -0500

    working navigation and minor breakpoint area fixes

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ import content from "./modules/nav.js";
 // Select nav and add the nav content from the module
 const nav = document.querySelector(".nav");
 nav.innerHTML = content;
+
+let burger = document.querySelector(".burger");
+let menu = document.querySelector(".hidden-nav");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
--- js/modules/nav.js
+++ js/modules/nav.js
@@ -23,6 +23,22 @@ const content = `
     <li><a href="${links.three}" class="link">${navItems.linkThree}</a></li>
     <li><a href="${links.four}" class="link">${navItems.linkFour}</a></li>
   </ul>
+
+  <div class="burger">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+
+  <div class="hidden-nav">
+    <ul class="nav-links-h">
+      <li><a href="${links.one}" class="link">${navItems.linkOne}</a></li>
+      <li><a href="${links.two}" class="link">${navItems.linkTwo}</a></li>
+      <li><a href="${links.three}" class="link">${navItems.linkThree}</a></li>
+      <li><a href="${links.four}" class="link">${navItems.linkFour}</a></li>
+    </ul>
+  </div>
+
 </div>
 `;
 
